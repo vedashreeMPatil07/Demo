77364cd02b5a42f53f1acd46ea727a331977e90b
vedashreeMPatil <vedashreepatil07@gmail.com>
2024-05-12 22:58:32 +0530

    initial init:

---
+++ pattern/Pattern2.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+	int i,j,k;
+	for(i=1;i<=5;i++) {
+		for(j=1;j<1;j++) {
+			System.out.print(" ");
+		}
+		for(k=i;k<=5;k++) {
+			System.out.print("*");
+		}
+		System.out.print("\n");
+	}
+
+	}
+
+}
